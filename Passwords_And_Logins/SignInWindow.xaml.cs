20de9ae065ab35e036e2d472b6156e207dea2f61
Pirog <vadim.pirog007@gmail.com>
2019-06-10 14:53:01 +0300

    Login and Password field

--- Passwords_And_Logins/SignInWindow.xaml.cs
+++ Passwords_And_Logins/SignInWindow.xaml.cs
@@ -10,29 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Passwords_And_Logins
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
-        }
-
-        private void Sign_In_Click(object sender, RoutedEventArgs e)
-        {
-            SignInBtn.IsEnabled = false;
-            SignUpBtn.IsEnabled = false;
-
-            Window1 signInWindow = new Window1();
-            signInWindow.Show();
-
         }
     }
 }
